cc21bef0d80b52c4b937d84fcea461fa44cd9bac
Ander Conal <ander.conal@plainconcepts.com>
2024-06-12 16:39:21 +0200

    🔧 Configure static code analysis for TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    indent: ["off"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": "off",
+    "class-methods-use-this": "off",
+    "object-curly-spacing": ["error", "always"],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "off",
+      {
+        accessibility: "explicit",
+        overrides: {
+          constructors: "no-public",
+        },
+      },
     ],
+    "react/react-in-jsx-scope": "off",
   },
-}
+};  
