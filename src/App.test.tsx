e35ac3d3b3ba5ce22c6334897492ce56edb9177b
Ander Conal <ander.conal@plainconcepts.com>
2024-06-12 17:47:06 +0200

    🧪 Add test example testing DOM nodes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,37 @@
-import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
-  });
+test("renders Vite Logo link and image", () => {
+  render(<App />);
+  
+  expect(screen.getByRole("link", { name: /Vite Logo/i })).toBeInTheDocument();
+  expect(screen.getByRole("img", { name: /Vite Logo/i })).toBeInTheDocument();
+});
 
-  it("false to be false", () => {
-    expect(false).toBe(false);
-  });
+test("renders React Logo link and image", () => {
+  render(<App />);
+  
+  expect(screen.getByRole("link", { name: /React Logo/i })).toBeInTheDocument();
+  expect(screen.getByRole("img", { name: /React Logo/i })).toBeInTheDocument();
+});
+
+test("renders Vite + React heading", () => {
+  render(<App />);
+  
+  expect(screen.getByRole("heading", { level: 1, name: "Vite + React" })).toBeInTheDocument();
+});
+
+test("Counter increments on button click", async () => {
+  const user = userEvent.setup();
+
+  render(<App />);
+  
+  await user.click(screen.getByRole("button", { name: /count is 0/i }));
+
+  expect(screen.getByRole("button", { name: /count is 1/i })).toBeInTheDocument();
+
+  await user.click(screen.getByRole("button", { name: /count is 1/i }));
+
+  expect(screen.getByRole("button", { name: /count is 2/i })).toBeInTheDocument();
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./tests/setup.ts", "reflect-metadata"],
+    coverage: {
+      provider: "v8",
+      reporter: ["cobertura"],
+    },
+  },
 });
