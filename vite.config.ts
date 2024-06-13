6eea17ebdecb33a1d2d143eea66c9ecd983709da
Ander Conal <ander.conal@plainconcepts.com>
2024-06-13 14:42:00 +0200

    🔧 Add base URL to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://anderconal.github.io/explore-the-data-app/",
   test: {
     globals: true,
     environment: "happy-dom",
